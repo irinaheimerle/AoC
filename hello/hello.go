d5a9b01f317ce0acd1e80d8c6fc56503208ca4a1
Rin Heimerle <rheimerle@rheimerle2019spare.local>
2020-04-20 11:17:17 -0300

    Starting with the first Go tutorial

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+// first statement in a Go source file must be package name
+// executable commands must always use package main
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world")
+}
